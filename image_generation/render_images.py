3749fa5707a9783f62bbeed1f052b113a16b3e35
gautam <singh.gautam.iitg@gmail.com>
2021-09-01 17:25:52 -0400

    test these scenes

--- image_generation/render_images.py
+++ image_generation/render_images.py
@@ -74,7 +74,7 @@ parser.add_argument('--margin', default=0.4, type=float,
     help="Along all cardinal directions (left, right, front, back), all " +
          "objects will be at least this distance apart. This makes resolving " +
          "spatial relationships slightly less ambiguous.")
-parser.add_argument('--min_pixels_per_object', default=400, type=int,
+parser.add_argument('--min_pixels_per_object', default=200, type=int,
     help="All objects will have at least this many visible pixels in the " +
          "final rendered images; this ensures that no objects are fully " +
          "occluded by other objects.")
