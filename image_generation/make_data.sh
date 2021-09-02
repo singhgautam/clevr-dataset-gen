27483b5b3c5fc9fb5e7ab8972024ae1f025c1b44
gautam <singh.gautam.iitg@gmail.com>
2021-09-02 11:52:20 -0400

    make async data

--- image_generation/make_data.sh
+++ image_generation/make_data.sh
@@ -1,4 +1,4 @@
 for ((n=0;n<200;n++))
 do
- ~/blender/blender --background --python render_images.py -- --num_images 100
+/common/home/gs790/blender/blender --background --python render_images.py -- --num_images 100
 done
