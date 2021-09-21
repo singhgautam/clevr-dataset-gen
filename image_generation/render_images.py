372527225d9ff9502c8e9de07b48a49c4317a2dc
gautam <singh.gautam.iitg@gmail.com>
2021-09-21 00:00:52 -0400

    testing

--- image_generation/render_images.py
+++ image_generation/render_images.py
@@ -356,7 +356,7 @@ def add_random_objects(scene_struct, num_objects, args, camera):
   mirror_theta = 180
   mirror_x = 3 * math.sqrt(2) * math.cos(mirror_theta * math.pi / 180.)
   mirror_y = 3 * math.sqrt(2) * math.sin(mirror_theta * math.pi / 180.)
-  utils.add_object(args.shape_dir, "Mirror", 2, (0, 4), theta=135)
+  utils.add_object(args.shape_dir, "Mirror", 2, (0, 4), theta=45)
   obj = bpy.context.object
   blender_objects.append(obj)
   utils.add_material("MyMetal", Color=color_name_to_rgba["gray"])
