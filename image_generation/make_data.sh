abf5751ffb3f77de6f70480bab7c2a40e267f2e2
gautam <singh.gautam.iitg@gmail.com>
2022-08-30 16:14:49 -0400

    make data 300

--- image_generation/make_data.sh
+++ image_generation/make_data.sh
@@ -1,5 +1,5 @@
 i=0
-while [ "$i" -le 200 ]; do
+while [ "$i" -le 300 ]; do
     /common/home/gs790/blender/blender --background --python render_images.py -- --num_images 100
     i=$(( i + 1 ))
 done
