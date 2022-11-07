d2fa8179bd5a00c70aff9f5bfbbb6273602ca79d
gautam <singh.gautam.iitg@gmail.com>
2022-11-07 17:19:49 -0500

    no consumer goods

--- image_generation/data/gso/download_gso.py
+++ image_generation/data/gso/download_gso.py
@@ -14,7 +14,7 @@ with open(gso_json) as json_file:
     for i, item in tqdm(enumerate(items)):
         category = data['assets'][item]['metadata']['category']
         category_counts[category] += 1
-        if category in ['Shoe', 'Bottles and Cans and Cups', 'Consumer Goods']:
+        if category in ['Shoe', 'Bottles and Cans and Cups']:
             os.system(f"wget https://storage.googleapis.com/kubric-public/assets/GSO/{item}.tar.gz")
             os.system(f"mkdir {item}")
             os.system(f"tar -xf {item}.tar.gz -C {item}")
