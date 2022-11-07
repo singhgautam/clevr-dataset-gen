4d138fe8c2309fbc9a38acfc9ce62263a96f5d10
gautam <singh.gautam.iitg@gmail.com>
2022-11-07 13:02:48 -0500

    testing

--- image_generation/utils.py
+++ image_generation/utils.py
@@ -116,8 +116,6 @@ def add_object(filename, name, scale, loc, theta=0):
   blender_obj.location[1] = 0
   blender_obj.location[2] = 0
 
-  import pdb; pdb.set_trace()
-
   bpy.context.scene.objects.active = blender_obj
 
   # bpy.ops.transform.resize(value=(scale, scale, scale))
